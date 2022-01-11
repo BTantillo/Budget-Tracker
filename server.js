314abcd15af12224d77dbd88fa2ab206b7b26c30
Mountaineer Ben <bentantillo@gmail.com>
2022-01-11 14:00:57 -0800

    data cleared

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
 const compression = require("compression");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
